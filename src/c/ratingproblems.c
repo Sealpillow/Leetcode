b683a7bd8e023e54dc4b8493492a13be0f499b02
Sealpillow <51332449+Sealpillow@users.noreply.github.com>
2021-06-15 10:12:46 +0800

    Create ratingproblems.c

---
+++ src/c/ratingproblems.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int num,done,rating,total=0;
+    scanf("%d %d",&num,&done);
+    for(int i=0;i<done;i++)
+    {
+        scanf("%d",&rating);
+        total = total+rating;
+    }
+    float min_final_rating = (total+(num-done)*-3.0)/num;
+    float max_final_rating = (total+(num-done)*3.0)/num;
+    printf("%g %g",min_final_rating,max_final_rating); //%g remove trailing zeros
+}
