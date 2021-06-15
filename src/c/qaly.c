3fc89c7e3ef53e092f5896001fa4ef2c31ab8c6e
Sealpillow <51332449+Sealpillow@users.noreply.github.com>
2021-06-15 10:10:13 +0800

    Create qaly.c

---
+++ src/c/qaly.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int num;
+    float total=0,quality,years;
+    scanf("%d",&num);
+    for(int i=0;i<num;i++)
+    {
+        scanf("%f %f",&quality,&years);
+        total = total+quality*years;
+    }
+    printf("%.3f",total);
+
+}
