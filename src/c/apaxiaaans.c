0baa6b30a69e7c9f6b1b231e4b5ca046137fe391
Sealpillow <51332449+Sealpillow@users.noreply.github.com>
2021-06-15 09:34:29 +0800

    Create apaxiaaans.c

---
+++ src/c/apaxiaaans.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<string.h>
+
+int main(void) {
+    char word[250];
+    scanf("%s",&word);
+    int n = strlen(word);
+    char prev=word[0];
+    for(int i=1;i<n;i++)
+    {
+        while(word[i]==prev)
+        {
+            for(int x=i;x<n;x++)
+            {
+                word[x]=word[x+1];
+                n=strlen(word);
+            }
+        }
+        prev=word[i];
+    }
+    printf("%s\n",word);
+    return 0;
+}
