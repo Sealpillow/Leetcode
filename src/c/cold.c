69e991b4a7f478ca6d4c15ce83b908fe55fbb811
Sealpillow <51332449+Sealpillow@users.noreply.github.com>
2021-06-15 09:25:34 +0800

    Create cold.c

---
+++ src/c/cold.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    int num,temp,total=0;
+    scanf("%d",&num);
+    for(int i=0;i<num;i++)
+    {
+        scanf("%d",&temp);
+        if(temp<0)
+        {
+            total++;
+        }
+    }
+    printf("%d",total);
+    return 0;
+}
