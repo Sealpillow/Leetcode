66261f4eda52e3a977b2d95513b4ecba212c096a
Sealpillow <51332449+Sealpillow@users.noreply.github.com>
2021-07-11 17:28:54 +0800

    Update lastfactorialdigit.c

--- src/c/junk/lastfactorialdigit.c
+++ src/c/junk/lastfactorialdigit.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
 
 int main() {
 
     int t,n,sum[10]={0};
     scanf("%d",&t);
-    for(int i=1;i<t+1;i++)
+    for(int i=0;i<t;i++)
     {
-        sum[i]++;
+        sum[i]++; //set all variable in array sum[i]={1};
         scanf("%d",&n);
 
         for(int x=1;x<n+1;x++)
         {
-            sum[i]=sum[i]*x;
+            sum[i]=sum[i]*x; // i*x, x as increment of 1
             printf("i:%d x: %d s:%d\n",i,x,sum[i]);
         }
     }
-    for(int i=1;i<t+1;i++)
+    for(int i=0;i<t;i++)
     {
-        printf("%d\n",sum[i]);
+        printf("%d\n",sum[i]%10); //last digit of the sum,hence ur remainder to find the ones digit.
     }
-    
     
     return 0;
 }
