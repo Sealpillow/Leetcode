29464cea96019dd05ea9603932a4f35cd70feab8
Sealpillow <51332449+Sealpillow@users.noreply.github.com>
2021-07-13 17:32:27 +0800

    Update conundrum.c

--- src/c/junk/conundrum.c
+++ src/c/junk/conundrum.c
@@ -3,13 +3,14 @@
 
 int main() {
 
-    int length,day=0;
+    int length,r,day=0;
     char string[300];
     scanf("%s",string);
     length= strlen(string);
     for(int i=0;i<length;i++)
     {
-        if((i+1)%3==1)//p
+        r=i%3;//remainder to check letter based on position
+        if(r==0)//p
         {
             if(string[i]!='P')
             {
@@ -17,7 +18,7 @@ int main() {
                 day++;
             }    
         }
-        else if((i+1)%3==2)//e
+        else if(r==1)//e
         {
             if(string[i]!='E')
             {
@@ -25,15 +26,14 @@ int main() {
                 day++;
             } 
         }
-        else if ((i+1)%3==0)
+        else if (r==2)
         {
             if(string[i]!='R')
             {
                 printf("%c",string[i]);
                 day++;
             } 
-        }
-        
+        } 
     }
     printf("%d",day);
 
