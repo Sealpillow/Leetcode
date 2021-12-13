6f8e6b29c73f010d8abd25c5957c6f55b04c1a79
Sealpillow <51332449+Sealpillow@users.noreply.github.com>
2021-12-14 01:19:21 +0800

    Create grading.c

---
+++ src/c/grading.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int a,b,c,d,e,score;
+    scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+    scanf("%d",&score);
+    if(score>=a)
+    {
+        printf("A");
+    }
+    else if(score>=b)
+    {
+        printf("B");
+    }
+    else if(score>=c)
+    {
+        printf("C");
+    }
+    else if(score>=d)
+    {
+        printf("D");
+    }
+    else if(score>=e)
+    {
+        printf("E");
+    }
+    else
+    {
+        printf("F");
+    }
+    return 0;
+}
