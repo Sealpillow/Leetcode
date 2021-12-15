5f5441a415366ed169b6a31d9a98acfb29d3cf2f
Sealpillow <51332449+Sealpillow@users.noreply.github.com>
2021-12-15 14:03:06 +0800

    Update pyramids.c

--- src/c/junk/pyramids.c
+++ src/c/junk/pyramids.c
@@ -1,14 +1,27 @@
+int main() {
+
+    int n;
+    scanf("%d",&n);
+    printf("%d",rec(n));
+    return 0;
+}
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
 int i=1,total=0;
+/*
+    total = 0 + 1   (1x1)
+    total = 1 + 9   (3x3)
+    total = 10 + 25  (5x5)
+
+*/
 int rec(int x)
 {
-    total += (1+i*2)*(1+i*2); // total number of block based on step
+    total += (1+i*2)*(1+i*2); // total number of block with addition to step,
     printf("%d\n",total);
-    if(x>total) // if the input > total block based on step, recursive continues
+    if(x>total) // if the input > total block based on step, recursive continues e.g 83>10 hence continues
     {
         i++; // increase step value
         return rec(x);
@@ -19,8 +32,6 @@ int rec(int x)
     }
 
 }
-
-
 
 int main() {
 
