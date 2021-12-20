41ed62de9b46b5f26ae04455af5d8fadb2c3b6ba
Sealpillow <51332449+Sealpillow@users.noreply.github.com>
2021-12-20 15:15:35 +0800

    Create apaxianparent.c

---
+++ src/c/apaxianparent.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+int main()
+{
+    int len;
+    double x=0,y=0; // value is big hence double instead of float
+    char input1[30],input2[30];
+    scanf("%s %s",input1,input2);
+    len = strlen(input1);
+    if(input1[len-1]=='a'||input1[len-1]=='i'||input1[len-1]=='o'||input1[len-1]=='u')
+    {
+        input1[len-1]='\0';
+        strcat(input1,"ex");
+        strcat(input1,input2);
+    }
+    else if(input1[len-1]=='e')
+    {
+        strcat(input1,"x");
+        strcat(input1,input2);
+    }
+    else if(input1[len-2]=='e'&&input1[len-1]=='x')
+    {
+        strcat(input1,input2);
+    }
+    else
+    {
+        strcat(input1,"ex");
+        strcat(input1,input2);
+    }
+    printf("%s",input1);
+}
