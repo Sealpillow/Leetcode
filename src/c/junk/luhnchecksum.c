4f5f5b5811ef6a5dbe3ef9426a7096837d32ec07
Sealpillow <51332449+Sealpillow@users.noreply.github.com>
2021-12-24 17:49:04 +0800

    Update luhnchecksum.c

--- src/c/junk/luhnchecksum.c
+++ src/c/junk/luhnchecksum.c
@@ -5,20 +5,35 @@
 
 int main()
 {
-    int n,sum=0,d,index=0;
+    int n,sum,d,index;
     long long num;
     scanf("%d",&n);
     for(int i=0;i<n;i++)
     {
+        index =0;
+        sum=0;
         scanf("%lld",&num);
         while(num!=0)
         {
+
             d=num%10;
             num=num/10;
             printf("num: %lld\n",num);
             if(index%2!=0)
             {
+                d*=2;
                 printf("index:%d d:%d\n",index,d);
+                if(d>9)
+                {
+                    sum+=d%10 + d/10;
+                }
+                else
+                {
+                   sum+=d;
+                }
+            }
+            else
+            {
                 sum+=d;
             }
             printf("%d\n",sum);
