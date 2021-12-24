70f2a1eca0d307d3e2e8da05b2c192a2d2984cb2
Sealpillow <51332449+Sealpillow@users.noreply.github.com>
2021-12-24 17:51:55 +0800

    Update luhnchecksum.c

--- src/c/junk/luhnchecksum.c
+++ src/c/junk/luhnchecksum.c
@@ -6,7 +6,7 @@
 int main()
 {
     int n,sum,d,index;
-    long long num;
+    long long num; //long number of inputs
     scanf("%d",&n);
     for(int i=0;i<n;i++)
     {
@@ -15,31 +15,30 @@ int main()
         scanf("%lld",&num);
         while(num!=0)
         {
-
             d=num%10;
             num=num/10;
             printf("num: %lld\n",num);
-            if(index%2!=0)
+            if(index%2!=0) //if index is odd
             {
                 d*=2;
                 printf("index:%d d:%d\n",index,d);
-                if(d>9)
+                if(d>9) //when digit after 2x is more than 9, get the sum of the 2 digit
                 {
                     sum+=d%10 + d/10;
                 }
-                else
+                else //if digit is less than 9 just add it to sum
                 {
                    sum+=d;
                 }
             }
-            else
+            else //if index is odd
             {
                 sum+=d;
             }
             printf("%d\n",sum);
             index++;
         }
-        if(sum*2%10==0)
+        if(sum*2%10==0) // if total sum is multiple of 10, it pass, else fail
         {
             printf("PASS\n");
         }
