5c6613625c67c720316e99b5d648ff156eba3c2d
Sealpillow <51332449+Sealpillow@users.noreply.github.com>
2021-12-31 16:00:44 +0800

    Update racingalphabet.c

--- src/c/junk/racingalphabet.c
+++ src/c/junk/racingalphabet.c
@@ -1,28 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <math.h>
-#define PI 3.142857
-#define dia 60
-// count the number of each alpha in the sentence.
-// Start position will be the first letter of the sentence
-int main() {
+#include <string.h>
+/*
+ACA: A->C
+      <-
+2 grids from A to C
+2 grids back from C to A
 
-    int n;
+We start of by getting the whole input into an array,
+set the first char as the prev_index
+compare the index between the prev and the current
+the difference will be the number of grid the player travel.
+for space and apostrophe, I've set them index 27,28 respectively as per order on the board
+
+
+
+
+
+*/
+int get_index(char c)
+{
+     if(c==' ')
+    {
+        return 26;
+    }
+    else if(c=='\'')
+    {
+        return 27;
+    }
+    else
+    {
+        return c-'A';
+    }
+}
+int main()
+{
+    double grid_dist= 3.14159265359*60/28,time;
+    //each pick up will take 1 sec, so how many alpha = + number of sec
+    char user[200],*p;
+    int grids=0,n;
     scanf("%d",&n);
-    char word[20][121],*p, alpha[26];
+    getchar();
     for(int i=0;i<n;i++)
     {
-        fgets(word[i],121,stdin);
-        if(p=strchr(word[i],'\n'))
+        fgets(user,200,stdin);
+        if(p=strchr(user,'\n'))
         {
             *p='\0';
         }
+        int len = strlen(user);
+        int index, prev_index= get_index(user[0]);
+        for(int j=1;j<len;j++)
+        {
+            index = get_index(user[j]);
+            printf("%c %c\n",prev_index+'A',user[j]);// ]->'
+            int diff = abs(prev_index-index);
+            if(diff>14)
+            {
+                //when diff exceed by half it will go the opposite way,as it would be shorter
+                //eg. A to ' move 1 grid instead of going one big round, 27 grids
+                grids+= (28-diff);
+            }
+            else
+            {
+                grids+= diff;
+            }
+            prev_index=index;
+            printf("%d\n",grids);
+        }
+        // time = total distance/speed + number of alphabets(as 1 alphabet = 1sec)
+        time= (grids*grid_dist/15+len);
+        printf("%.9lf\n",time);
     }
-    cycle = PI*dia;
-    p = cycle /28; //per space
-    
-    time = dist/s;
-    printf("%d",rec(n));
-    return 0;
 }
