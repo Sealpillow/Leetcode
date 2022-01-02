55791602989caa6d8b1c650aad609863234e6c03
Sealpillow <51332449+Sealpillow@users.noreply.github.com>
2022-01-02 23:12:39 +0800

    Update drunkvigenere.c

--- src/c/junk/drunkvigenere.c
+++ src/c/junk/drunkvigenere.c
@@ -8,27 +8,36 @@
 
 int main()
 {
-    int index,len;
-    char encrypted[201],key[201],msg[201];
+    int index,len,c=0;
+    char encrypted[202],key[202],msg,*p;
     scanf("%s",encrypted);
     scanf("%s",key);
     len = strlen(encrypted);
     for(int i=0;i<len;i++)
     {
+        int index1= encrypted[i]-'A';
+        int index2= key[i]- 'A';
         if(i%2==0)
         {
             //since shift forward msg[i] + key[i] = encrypted[i],
             // therefore msg[i] = encrypted[i] - key[i] e.g R= abs(A-R),20= abs(-20)
-            index = abs(encrypted[i]-'A'-(key[i]-'A'));
+            index = (index1-index2);
         }
         else
         {
-            // since shift backwards msg[i] - key[i] = encrypted[i], 
+            // since shift backwards msg[i] - key[i] = encrypted[i],
             // therefore msg[i] = encrypted[i] + key[i],%26 as it loops
-            index = ((encrypted[i]-'A')+(key[i]-'A'))%26; 
+            index = (index1+index2)%26;
         }
-        printf("%d %c %c %d\n",i,encrypted[i],key[i],index);
-        msg[i] = index+'A';
+        printf("%d %d",index1,index2);
+        if(index<0) //in a case where by (3-5)=-2 it will have to (26-index)shift back loop
+        {
+            msg= (26+index) +'A';
+        }
+        else
+        {
+            msg = index + 'A';
+        }
+        putchar(msg);
     }
-    printf("%s",msg);
 }
