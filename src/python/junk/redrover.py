e14a1c97d4ec8a829ac49ba8f797deec2e9029b7
Sealpillow <51332449+Sealpillow@users.noreply.github.com>
2022-01-04 19:09:24 +0800

    Update redrover.py

--- src/python/junk/redrover.py
+++ src/python/junk/redrover.py
@@ -1,16 +1,16 @@
 from math import*
 """
-WNEENWEENEENE <user input
+WNEENWEENEENE <-user input
 
 macro     final
-E       WNMMNWMMNMMNM
-16
-NE      WMENWEEMEM
-13
-EEN     WNMWMME
+E         WNMMNWMMNMMNM
+14
+NE        WMENWEEMEM
+12
+EEN       WNMWMME
 10
-NEEN    WMWEEME
-10
+NEEN      WMWEEME
+11
 
 10  <-print the least number of char used
 
@@ -19,21 +19,24 @@ m = 1  # 1 occurrence
 final = ""
 macro = ""
 user = input()
-n = ceil(len(user)/2)
-min_char = 100
-for i in range(1, n):  # increase range [1:2]
-    m = 0
-    for j in range(n):
+min_char = len(user)  # set max length of char to min_char
+n = ceil(min_char/2)
+for i in range(1, n+1):  # in case where input: E, range(1,1+1) it can go into loop
+    # i is to increase range [1:2]... [1:9]->increase array length to check for repeats in user
+    m = 0  # reset max occurrence of a string length
+    for j in range(n):  # j-change position [0:1]..[1:2]..[5:8] is to move to every position in string
         # using slicing method
         count = user.count(user[j:j+i])  # to count number of occurrence of user[j:j+1] in user
         if count > m:  # finding max occurrence
-            macro = user[j:j+i]
+            macro = user[j:j+i]  # set the string of word into macro
             m = count  # replace max occurrence
             final = user.replace(str(macro), "M")  # replace the 3 letter(macro) into M
-    if m > 1:  # if max occurrence is more than 1, there is a macro in use
-        num = len(final) + 3
-        print(macro+" "+final)
+    if m > 1:  # if max occurrence of a string length is more than 1, there is a macro in use
+        num = len(final) + len(macro)
+        print(macro + " " + final)
         print(num)
-        if num < min_char:
-            min_char = num
+    else:
+        num = len(user)
+    if num < min_char:
+        min_char = num
 print(min_char)
