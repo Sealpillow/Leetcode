24c578ba8c1e9abc097bb547ec9d0ca8d5b4e049
Sealpillow <51332449+Sealpillow@users.noreply.github.com>
2022-01-12 20:44:36 +0800

    Update sok.py

--- src/python/sok.py
+++ src/python/sok.py
@@ -15,8 +15,3 @@ a,b,c=map(int,input().split())
 i,j,k=map(int,input().split())
 multiple=min(a/i,b/j,c/k)
 print(a-i*multiple,b-j*multiple,c-k*multiple)
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <string.h>
-
