65fc6cf012fa1120e9025082bfddde431bad6560
Fluffypillow <51332449+Sealpillow@users.noreply.github.com>
2022-07-09 02:17:40 +0800

    Create flyingsafely.c

---
+++ src/c/flyingsafely.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main()
+{
+    int cities,pilots,num,*arr,a,b,count,found,min;
+    scanf("%d",&num);
+    for(int i =0;i<num;i++)
+    {
+        scanf("%d %d",&cities,&pilots);
+        printf("%d\n",cities-1);
+        for(int j=0;j<pilots;j++)
+        {
+            scanf("%d %d",&a,&b);
+        }
+    }
+}
