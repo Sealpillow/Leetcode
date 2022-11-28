56987e6c8e6f319124c606e9fea03a083afea436
Fluffypillow <51332449+Sealpillow@users.noreply.github.com>
2022-11-28 22:25:19 +0800

    Update twoSum.py

--- src/python/twoSum.py
+++ src/python/twoSum.py
@@ -1,1 +1,9 @@
-
+class Solution(object):
+    def twoSum(self, nums, target):
+        set = []
+        for i in range(len(nums)):
+            for j in range(i, len(nums)):
+                if nums[i] + nums[j] == target and i != j:
+                    set.append(i)
+                    set.append(j)
+                    return set
