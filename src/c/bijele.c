f79defdedf5f37a0a6ecc7749e0160eb15bd4db1
Fluffypillow <51332449+Sealpillow@users.noreply.github.com>
2022-11-28 21:53:38 +0800

    Delete src/c directory

--- src/c/bijele.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int pi[6]={1,1,2,2,2,8},p,p1,p2,p3,p4,p5;
-    scanf("%d %d %d %d %d %d",&p,&p1,&p2,&p3,&p4,&p5);
-    printf("%d %d %d %d %d %d",pi[0]-p,pi[1]-p1,pi[2]-p2,pi[3]-p3,pi[4]-p4,pi[5]-p5);
-    return 0;
-}
--- src/c/countingclauses.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main()
-{
-    int m,n;
-    scanf("%d %d",&m,&n);
-    printf(m<8?"unsatisfactory":"satisfactory");
-}
