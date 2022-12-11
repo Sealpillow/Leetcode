1856cfc07645cb895beb012e5185acc7ccdc4591
Fluffypillow <51332449+Sealpillow@users.noreply.github.com>
2022-12-11 19:32:34 +0800

    Add files via upload

---
+++ src/java/TwoSum.java
@@ -0,0 +1,25 @@
+package leetcode.Easy;
+
+import java.util.Arrays;
+public class TwoSum
+{
+    public static int[] twoSum(int[] nums, int target)
+    {
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=0;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target && i!=j)
+                    return new int[]{i,j};
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args)
+    {
+        int[] nums = {2,3,4,0};
+        int target = 5;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+}
